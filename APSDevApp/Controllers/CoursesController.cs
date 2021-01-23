8a8bd0aa6f49406f64125c31981e32a8e1b7d25f
thanhthiengcd18708 <66301633+thanhthiengcd18708@users.noreply.github.com>
2021-01-23 17:31:17 +0700

    Add project files.

---
+++ APSDevApp/Controllers/CoursesController.cs
@@ -0,0 +1,32 @@
+﻿using APSDevApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APSDevApp.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        List<Course> _courses = new List<Course>
+        {
+            new Course (1,"GCD" ,"asds"),
+            new Course (2, "GBD","adddd"),
+            new Course( 3,"GED","adad" )
+        };
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            return View(_courses);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Course courseInDb = _courses.SingleOrDefault(t => t.Id == id);
+            return View(courseInDb);
+        }
+    }
+}
